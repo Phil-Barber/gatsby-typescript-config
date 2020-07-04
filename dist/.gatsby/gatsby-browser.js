7cd09a94325aad60bc0b151cf551ca8312e1a83e
Phil Barber <phil@lantum.com>
2020-07-04 15:47:50 +0100

    fix js

---
+++ dist/.gatsby/gatsby-browser.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("normalize.css");
+require("typeface-domine");
+require("typeface-montserrat");
+require("prismjs/themes/prism-okaidia.css");
+//# sourceMappingURL=gatsby-browser.js.map
---
+++ dist/.gatsby/gatsby-ssr.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=gatsby-ssr.js.map
